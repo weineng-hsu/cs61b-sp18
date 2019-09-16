2a7e8ee6f9e3382226e587883302eb281b98a0a9
wnhsu <wnhsu@wnhsude-MacBook-Pro.local>
2019-09-16 11:42:03 +0800

    finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,33 +1,58 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
- */
+/** this is a java tell year is a leap year or not
+*    @author WNSHU
+*/
 public class LeapYear {
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
-     */
-    private static void checkLeapYear(int year) {
-        if (isLeapYear(year)) {
-            System.out.printf("%d is a leap year.\n", year);
-        } else {
-            System.out.printf("%d is not a leap year.\n", year);
-        }
-    }
+	public static void checkLeapYear (int year) {
 
-    /** Must be provided an integer as a command line argument ARGS. */
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Please enter command line arguments.");
-            System.out.println("e.g. java Year 2000");
-        }
-        for (int i = 0; i < args.length; i++) {
-            try {
-                int year = Integer.parseInt(args[i]);
-                checkLeapYear(year);
-            } catch (NumberFormatException e) {
-                System.out.printf("%s is not a valid number.\n", args[i]);
-            }
-        }
-    }
+		if (isLeapYear(year) == true ) {
+			System.out.printf("%d is a leap year.\n", year);
+		}
+		else if (isLeapYear(year) == false) {
+			System.out.printf("%d is not a leap year.\n", year);
+		}
+	}
+
+	public static boolean isLeapYear (int year) {
+
+		if (year % 400 ==0) {
+			return true;
+		}else if (year % 4 ==0 & year % 100 !=0){
+			return true;
+		}else return false;
+	}
+
+
+	public static void main (String[] args) {
+		if (args.length<1) {
+			System.out.println("Enter the integer to check leap year.");
+			System.out.println("e.g. java LeapYear 2004");
+		}
+
+		for (int i = 0; i < args.length; i++ ) {
+
+			try {
+				int year = Integer.parseInt(args[i]);
+				checkLeapYear(year);
+			}
+			catch (NumberFormatException e) {
+				System.out.printf("%s is not a valid number.\n", args[i]);
+			}
+
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
