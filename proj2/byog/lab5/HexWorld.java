ab58f7cfa005e3d74c7bd08e0d2eaf839f162403
wnhsu <wnhsu@wnhsude-MacBook-Pro.local>
2020-06-15 11:07:49 +0800

    modify lab5, 6

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -1,205 +1,125 @@
 package byog.lab5;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
-import java.awt.*;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
+
 public class HexWorld {
-    private static final int WIDTH = 60;
-    private static final int WIDE = 40;
-    //private static final long SEED = 7;
-    private static final Random RANDOM = new Random();
+    private static final int WIDTH = 45;
+    private static final int HEIGHT = 45;
 
-    // Position reference class for world x,y coordinate.
+
     private static class Position {
-        int xPos;
-        int yPos;
-        private Position(int x, int y) {
-            xPos = x;
-            yPos = y;
+        int x;
+        int y;
+        Position(int xPos, int yPos) {
+            x = xPos;
+            y = yPos;
+        }
+        public Position copyPos() {
+            return new Position(this.x, this.y);
+        }
+        public Position upperHexPos(int side) {
+            return new Position(this.x, this.y + 2 * side);
+        }
+        public Position sideLowerHexPos(int side) {
+            return new Position(this.x + 2 * side - 1, this.y - side);
+        }
+        public Position raw4HexPos(int side) {
+            return new Position(this.x + 3 * (2 * side - 1), this.y - side);
+        }
+        public Position sideUpperHexPos(int side) {
+            return new Position(this.x + 2 * side - 1, this.y + side);
+        }
+
+    }
+
+    public static TETile[][] initilize() {
+        TETile[][] hexWorld = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                hexWorld[x][y] = Tileset.NOTHING;
+            }
+        }
+        return hexWorld;
+    }
+
+    public static void drawSingleHex(Position p, int side, TETile[][] world, TETile terrain) {
+        Position lineStart = p.copyPos();
+        for (int i = 0; i < side; i++) {
+            drawALine(lineStart, side + 2 * i, world, terrain);
+            lineStart.x -= 1;
+            lineStart.y += 1;
+        }
+        for (int i = side - 1; i >= 0; i--) {
+            lineStart.x += 1;
+            drawALine(lineStart, side + 2 * i, world, terrain);
+            lineStart.y += 1;
         }
     }
 
-    /**Function return width of the hex in h-raw.
-     * @param h the height of the hex
-     * @param sides the size of the hex
-     * @return the raw width of the hex
-     */
-    private static int width(int h, int sides) {
-        int add = h;
-        if (h >= sides) {
-            add = (sides  - (h - sides + 1));
-        }
-        return sides + 2 * add;
-    }
-
-    @Test
-    public void testWidth() {
-        assertEquals(width(2,3) , 7);
-        assertEquals(width(4,4) , 10);
-        assertEquals(width(5,4) , 8);
-        assertEquals(width(5,3) , 3);
-    }
-
-    /**
-     * Computesrelative x coordinate of the leftmost tile in the ith
-     * row of a hexagon, assuming that the bottom row has an x-coordinate
-     * of zero. For example, if s = 3, and i = 2, this function
-     * returns -2, because the row 2 up from the bottom starts 2 to the left
-     * of the start position.
-     * @param h h-raw of the hex, where 0 is the bottom
-     * @param sides size of the hex
-     * @return
-     */
-
-    private static int startOfRaw(int h, int sides) {
-        int x = h;
-        if (h >= sides ) {
-            x = sides - 1 - (h - sides);
-        } return - x;
-    }
-
-    @Test
-    public void testStartOfRaw() {
-        assertEquals(startOfRaw(1,2) , -1);
-        assertEquals(startOfRaw(4,3) , -1);
-        assertEquals(startOfRaw(1, 3) , -1);
-    }
-
-    /** Draw one of the line of Hex in the world.
-     * @param world the world to drawHex
-     * @param width the line width to draw on
-     * @param t draw with what kind of tile
-     * @param p at which position to draw the line
-     */
-    private static void drawHexLine(TETile[][] world, int width, TETile t, Position p) {
-        for (int i = 0; i < width; i += 1) {
-            world [p.xPos + i][p.yPos] = t;
+    public static void drawALine(Position p, int s, TETile[][] world, TETile terrain) {
+        for (int i = 0; i < s; i++) {
+            world[p.x + i][p.y] = terrain;
         }
     }
 
-    /** Draw the Hex by point out the raw starting position and
-     * the width of that line
-     * @param world the world to drawHex
-     * @param sides size of the Hex to draw
-     * @param t draw with what kind of tile
-     * @param p at which position to draw the Hex
-     */
-    private static void drawAHex(TETile[][] world, int sides, TETile t, Position p) {
-        for (int h = 0; h < 2 * sides; h += 1) {
-            Position RawStart = new Position(p.xPos + startOfRaw(h , sides),p.yPos + h);
-            drawHexLine(world , width(h , sides), t, RawStart);
+    public static TETile randomTile() {
+        Random r = new Random();
+        int tileRan = r.nextInt(7);
+        switch (tileRan) {
+            case 0: return Tileset.FLOOR;
+            case 1: return Tileset.WALL;
+            case 2: return Tileset.GRASS;
+            case 3: return Tileset.SAND;
+            case 4: return Tileset.MOUNTAIN;
+            case 5: return Tileset.TREE;
+            case 6: return Tileset.WATER;
+            default: return Tileset.NOTHING;
         }
     }
 
-    /** Create 5 random fill in tile effect.
-     * @param world
-     * @param sides
-     * @param p
-     */
-    private static void randomDrawAHex(TETile[][] world, int sides, Position p) {
-        int hexNum = RANDOM.nextInt(5);
-        TETile tile;
-        switch (hexNum) {
-            case 0:
-                tile = Tileset.FLOOR;
-                break;
-            case 1:
-                tile = Tileset.FLOWER;
-                break;
-            case 2:
-                tile = Tileset.GRASS;
-                break;
-            case 3:
-                tile = Tileset.MOUNTAIN;
-                break;
-            case 4:
-                tile = Tileset.SAND;
-                break;
-            default:
-                tile = Tileset.NOTHING;
-                break;
+    public static void drawHexWorld(Position p, int side, TETile[][] world) {
+        int hexInRow = 3;
+        int rowIndex = 0;
+        Position origin = p.copyPos();
+        while (rowIndex < 3) {
+            drawHexInRaw(origin, side, hexInRow + rowIndex, world);
+            origin = origin.sideLowerHexPos(side);
+            rowIndex += 1;
         }
-        drawAHex(world, sides, tile, p);
-    }
-
-    /** Expand one hex to 19 hexes
-     *     3/ 4/ 5/ 4/ 3
-     *           %
-     *        %     %
-     *      %    %     %
-     *        %     %
-     *      %    %     %
-     *        %     %
-     *   p->%    %     %
-     *        %     %
-     *           %
-     * @param world the world to draw
-     * @param sides the size of the hex
-     * @param p the hex position show above
-     */
-    private static void expand(TETile[][] world, int sides, Position p) {
-        int xDiff = 2 * sides - 1;
-        for (int i = 0; i < 3; i += 1) {
-            Position xHex = new Position(p.xPos + i * xDiff, p.yPos - i * sides);
-            expandYHelper(world, sides, xHex,  3 + i );
-        }
-        for (int i = 0; i < 2; i += 1) {
-            Position xHex = new Position(p.xPos + (3 + i) * xDiff, p.yPos - (1 - i) * sides);
-            expandYHelper(world, sides, xHex, 4 - i );
+        origin = p.raw4HexPos(side);
+        rowIndex -= 2;
+        while (rowIndex >= 0) {
+            drawHexInRaw(origin, side, hexInRow + rowIndex, world);
+            origin = origin.sideUpperHexPos(side);
+            rowIndex -= 1;
         }
     }
-    /** Helper method of Expand, draw one column of the hex
-     *     3
-     *      %
-     *      %
-     *   p->%
-     * @param world the world to draw
-     * @param sides the size of the hex
-     * @param p the hex position show above
-     * @param yNumber the number to draw in one column
-     */
 
-    private static void expandYHelper(TETile[][] world, int sides, Position p, int yNumber) {
-        int yDiff = 2 * sides;
-        for (int i = 0; i < yNumber; i += 1) {
-            Position YHex = new Position(p.xPos, p.yPos + i * yDiff);
-            randomDrawAHex(world, sides, YHex);
+    public static void drawHexInRaw(Position p, int side, int count, TETile[][] world) {
+        Position upper = p.upperHexPos(side);
+        for (int i = 0; i < count; i++) {
+            TETile ranTile = randomTile();
+            drawSingleHex(upper, side, world, ranTile);
+            upper = upper.upperHexPos(side);
         }
     }
 
     public static void main(String[] args) {
-
         TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, WIDE);
-
-        TETile [][] world = new TETile[WIDTH][WIDE];
-
-        //To fill in the world with nothing tile.
-        for (int x = 0; x < WIDTH; x += 1) {
-            for (int y = 0; y < WIDE; y += 1) {
-                world[x][y] = Tileset.NOTHING;
-            }
-        }
-
-        //drawHexLine(world, 3, t, 5, 8);
-        //drawAHex(world, 3, t, 10, 8);
-        //randomDrawAHex(world,3, 10, 8);
-        Position p = new Position(10, 8);
-        expand(world,4, p);
-
-        // draws the world to the screen
+        ter.initialize(WIDTH, HEIGHT);
+        TETile[][] world = HexWorld.initilize();
+        Position origin = new Position(5, 5);
+        //drawALine(origin, 4, world, Tileset.WALL);
+        //drawSingleHex(origin, 4, world, Tileset.WALL);
+        //drawHexInRaw(origin, 3, 4, world);
+        drawHexWorld(origin, 3, world);
         ter.renderFrame(world);
     }
 }
-
-
-
--- proj2/byog/lab6/MemoryGame.java
+++ proj2/byog/lab6/MemoryGame.java
@@ -1,5 +1,6 @@
 package byog.lab6;
 
+
 import edu.princeton.cs.introcs.StdDraw;
 
 import java.awt.Color;
@@ -15,8 +16,8 @@ public class MemoryGame {
     private boolean playerTurn;
     private static final char[] CHARACTERS = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     private static final String[] ENCOURAGEMENT = {"You can do this!", "I believe in you!",
-                                                   "You got this!", "You're a star!", "Go Bears!",
-                                                   "Too easy for you!", "Wow, so impressive!"};
+            "You got this!", "You're a star!", "Go Bears!",
+            "Too easy for you!", "Wow, so impressive!"};
 
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -24,15 +25,20 @@ public class MemoryGame {
             return;
         }
 
-        int seed = Integer.parseInt(args[0]);
-
-
+        //int seed = Integer.parseInt(args[0]);
+        int seed = 8;
         MemoryGame game = new MemoryGame(40, 40, seed);
+
+        //String test = game.generateRandomString(5);
+        //System.out.println(test);
+        //game.drawFrame("test");
+        //game.flashSequence("test");
+        //game.solicitNCharsInput(5);
 
         game.startGame();
     }
 
-    public MemoryGame(int width, int height, int seed) {
+    public MemoryGame(int width, int height, int randSeed) {
         /* Sets up StdDraw so that it has a width by height grid of 16 by 16 squares as its canvas
          * Also sets up the scale so the top left is (0,0) and the bottom right is (width, height)
          */
@@ -43,101 +49,118 @@ public class MemoryGame {
         StdDraw.setFont(font);
         StdDraw.setXscale(0, this.width);
         StdDraw.setYscale(0, this.height);
-
         StdDraw.clear(Color.BLACK);
         StdDraw.enableDoubleBuffering();
 
         //TODO: Initialize random number generator
-        rand = new Random(seed);
+        rand = new Random(randSeed);
     }
 
     public String generateRandomString(int n) {
-        //TODO: Generate random string of letters of length n
-        StringBuilder RandomS = new StringBuilder();
-        for (int i = 0; i < n; i += 1) {
-            int Random = rand.nextInt(CHARACTERS.length);
-            RandomS.append(CHARACTERS[Random]);
+        //TODO: Generate random string of letters of length
+        StringBuilder genString = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int charIndex = rand.nextInt(CHARACTERS.length);
+            genString.append(CHARACTERS[charIndex]);
         }
-        return RandomS.toString();
+        return genString.toString();
     }
-
 
     public void drawFrame(String s) {
         //TODO: Take the string and display it in the center of the screen
         //TODO: If game is not over, display relevant game information at the top of the screen
-        double centerHeight = height / 2;
-        double centerWidth = width / 2;
-        StdDraw.setPenColor(Color.YELLOW);
-        StdDraw.text(centerWidth, centerHeight , s);
+        StdDraw.clear(Color.BLACK);
+        Font font = new Font ("Monaco", Font.BOLD, 30);
+        StdDraw.setFont(font);
+        StdDraw.setPenColor(Color.WHITE);
+        StdDraw.text(width / 2, height / 2, s);
+        StdDraw.show();
+
+        if (!gameOver) {
+            Font smallFont = new Font("Monaco", Font.BOLD, 20);
+            StdDraw.setFont(smallFont);
+            StdDraw.textLeft(1, height - 1, "Round: " + round);
+            StdDraw.text(width / 2, height - 1, playerTurn ? "Type!" : "Watch!");
+            StdDraw.textRight(width - 1, height - 1, ENCOURAGEMENT[round % ENCOURAGEMENT.length]);
+            StdDraw.line(0, height - 2, width, height - 2);
+        }
     }
 
     public void flashSequence(String letters) {
         //TODO: Display each character in letters, making sure to blank the screen between letters
-        char[] chars = letters.toCharArray();
-        StringBuilder print = new StringBuilder();
-        StdDraw.enableDoubleBuffering();
-        for (char add : chars) {
-            StdDraw.clear(Color.BLACK);
-            StdDraw.pause(500);
-            print.append(add);
-            this.drawFrame(print.toString());
-            StdDraw.text(height / 2, width / 2, print.toString());
+        for (int i = 0; i < letters.length(); i++) {
+            drawFrame(letters.substring(i, i + 1));
             StdDraw.show();
             StdDraw.pause(1000);
+            StdDraw.clear(Color.BLACK);
+            StdDraw.show();
+            StdDraw.pause(500);
         }
-        StdDraw.disableDoubleBuffering();
-        StdDraw.clear(Color.BLACK);
+        /*char[] toPrintChars = letters.toCharArray();
+        for (int i = 0; i < toPrintChars.length; i++) {
+            drawFrame(String.valueOf(toPrintChars[i]));
+            StdDraw.show();
+            StdDraw.pause(1000);
+            StdDraw.clear(Color.BLACK);
+            StdDraw.show();
+            StdDraw.pause(500);
+        }
+         */
     }
 
     public String solicitNCharsInput(int n) {
         //TODO: Read n letters of player input
-        StringBuilder typed = new StringBuilder();
-        for (int i = 0; i < n; i += 1) {
-            typed.append(StdDraw.nextKeyTyped());
+        /*StringBuilder input =  new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (!StdDraw.hasNextKeyTyped()) {
+                continue; //If no input return
+            }
+            input.append(StdDraw.nextKeyTyped());
+            drawFrame(input.toString());
+            StdDraw.pause(1000);
         }
-        return typed.toString();
-    }
+        return input.toString();
 
+         */
+        String input = "";
+        drawFrame(input);
+
+        while (input.length() < n) {
+            if (!StdDraw.hasNextKeyTyped()) {
+                continue;
+            }
+            char key = StdDraw.nextKeyTyped();
+            input += String.valueOf(key);
+            drawFrame(input);
+        }
+        StdDraw.pause(500);
+        return input;
+    }
 
     public void startGame() {
         //TODO: Set any relevant variables before the game starts
-        this.round = 1;
-        String showRound = "Round ";
-
-        //String show = generateRandomString(round);
-        //String input = solicitNCharsInput(round);
-
-        //Boolean gameOver = false;
-
-
-        //TODO: Establish Game loop
-        for (int n = 0; n < this.round; n += 1) {
-
-            this.drawFrame(showRound + round);
-            StdDraw.show();
-            StdDraw.pause(1000);
-            StdDraw.disableDoubleBuffering();
-            StdDraw.clear(Color.BLACK);
-            StdDraw.enableDoubleBuffering();
-            String show = generateRandomString(round);
-            this.flashSequence(show);
-            StdDraw.pause(5000);
-            String input = solicitNCharsInput(round);
-            this.flashSequence(input);
-            StdDraw.pause(500);
-            StdDraw.disableDoubleBuffering();
-            StdDraw.clear(Color.BLACK);
-            gameOver = !input.equals(show);
-            if (gameOver) {
-                this.drawFrame("Gameover");
-                break;
+        round = 1;
+        gameOver = false;
+        String randString;
+        String userAns;
+        while (!gameOver) {
+            drawFrame("Round: " + round);
+            StdDraw.pause(3000);
+            randString = generateRandomString(round);
+            flashSequence(randString);
+            StdDraw.pause(3000);
+            userAns = solicitNCharsInput(round);
+            drawFrame(userAns);
+            StdDraw.pause(3000);
+            if (!userAns.equals(randString)) {
+                int completed = round - 1;
+                drawFrame("Game Over! You made it to round: " + completed);
+                gameOver = true;
+            } else {
+                round += 1;
+                drawFrame("Correct, well done!");
+                StdDraw.pause(1500);
             }
-            this.round += 1;
         }
-
-
-
-
     }
-
 }
