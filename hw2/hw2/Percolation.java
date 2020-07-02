e0f05fc17452998ece6b248eb16f4844e93f95bf
wnhsu <wnhsu@wnhsude-MacBook-Pro.local>
2020-07-02 20:22:16 +0800

    finish HW2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,112 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    boolean[][] earth;
+    int side;
+    int openCount;
+    WeightedQuickUnionUF connectedGrids;
+    WeightedQuickUnionUF connectedGridsToTop;
+
+
+    public Percolation(int N) {
+        if (N < 0 || N == 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        side = N;
+        earth = new boolean[side][side];
+
+        connectedGrids = new WeightedQuickUnionUF(N * N + 2);
+        connectedGridsToTop = new WeightedQuickUnionUF(N * N + 1);
+        /*
+        for (int i = 0; i < N; i++) {
+            connectedGrids.union(N * N, xyTo1D(0, i));
+            connectedGridsToTop.union(N * N, xyTo1D(0, i));
+        }
+        for (int i = 0; i < N; i++) {
+            connectedGrids.union(N * N + 1, xyTo1D(N - 1, i));
+        }
+
+         */
+        this.clear();
+
+    }
+
+    private void clear() {
+        for (int x = 0; x < side; x++) {
+            for (int y = 0; y < side; y++) {
+                earth[x][y] = false;
+            }
+        }
+        openCount = 0;
+    }
+
+    public void open(int row, int col) {
+        if (!inputInGrid(row, col)) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        if (earth[row][col]) {
+            return;
+        }
+        earth[row][col] = true;
+        openCount += 1;
+        if (inputInGrid(row + 1, col)) {
+            if (isOpen(row + 1, col)) {
+                connectedGrids.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+                connectedGridsToTop.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            }
+        }
+        if (inputInGrid(row - 1, col)) {
+            if (isOpen(row - 1, col)) {
+                connectedGrids.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+                connectedGridsToTop.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+            }
+        }
+        if (inputInGrid(row, col + 1)) {
+            if (isOpen(row, col + 1)) {
+                connectedGrids.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+                connectedGridsToTop.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+            }
+        }
+        if (inputInGrid(row, col - 1)) {
+            if (isOpen(row, col - 1)) {
+                connectedGrids.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+                connectedGridsToTop.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            }
+        }
+        if (row == 0) {
+            connectedGrids.union(xyTo1D(row, col), side * side);
+            connectedGridsToTop.union(xyTo1D(row, col), side * side);
+        }
+        if (row == side - 1) {
+            connectedGrids.union(xyTo1D(row, col), side * side + 1);
+        }
+
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (!inputInGrid(row, col)) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        return earth[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        return connectedGridsToTop.connected(xyTo1D(row, col), side * side);
+    }
+
+    private int xyTo1D(int row, int col) {
+        return row * side + col;
+    }
+
+    private boolean inputInGrid(int row, int col) {
+        return (row >= 0 && row <= side - 1 && col >= 0 && col <= side - 1);
+    }
+
+    public int numberOfOpenSites() {
+        return openCount;
+    }
+
+    public boolean percolates() {
+        return connectedGrids.connected(side * side, side * side + 1);
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,45 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private int N;
+    private int T;
+    private double[] thresholds;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        N = N;
+        T = T;
+        for (int i = 0; i < T; i++) {
+            Percolation grids = pf.make(N);
+            while (!grids.percolates()) {
+                int ranX = StdRandom.uniform(N);
+                int ranY = StdRandom.uniform(N);
+                if (!grids.isOpen(ranX, ranY)) {
+                    grids.open(ranX, ranY);
+                }
+            }
+            thresholds[i] = grids.openCount;
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 }
