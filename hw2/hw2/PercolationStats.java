0beda689c0606e7379af0db9eabe928575ddedee
wnhsu <wnhsu@wnhsude-MacBook-Pro.local>
2020-07-02 23:15:20 +0800

    modify thresholds[]

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -14,7 +14,8 @@ public class PercolationStats {
         }
         this.N = N;
         this.T = T;
-        thresholds = new double[N];
+        thresholds = new double[T];
+
         for (int i = 0; i < T; i++) {
             Percolation grids = pf.make(N);
             while (!grids.percolates()) {
@@ -24,7 +25,7 @@ public class PercolationStats {
                     grids.open(ranX, ranY);
                 }
             }
-            double threshold = grids.numberOfOpenSites() / (N * N);
+            double threshold = (double) grids.numberOfOpenSites() / (N * N);
             thresholds[i] = threshold;
         }
     }
@@ -43,5 +44,16 @@ public class PercolationStats {
 
     public double confidenceHigh() {
         return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public static void main(String[] args) {
+        PercolationFactory pf = new PercolationFactory();
+        //PercolationStats test = new PercolationStats(20, 10, pf);
+        //PercolationStats test1 = new PercolationStats(50, 20, pf);
+        //PercolationStats test2 = new PercolationStats(100, 50, pf);
+        //PercolationStats test3 = new PercolationStats(50, 20, pf);
+        PercolationStats test4 = new PercolationStats(64, 150, pf);
+        System.out.println(test4.mean());
+        System.out.println(test4.confidenceHigh());
     }
 }
